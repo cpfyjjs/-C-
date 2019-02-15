9ff8b65c035f225adc3ccab5e2355dd0d01c9ef1
cpfyjjs <cpfyjjs@foxmail.com>
2019-02-15 18:09:53 +0800

    0215

--- 11泛型/11泛型/Program.cs
+++ 11泛型/11泛型/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace _11泛型
 {
-    class Program
+    partial class Program
     {
-        static void Main(string[] args)
+        static void Main01(string[] args)
         {
             MyStack<int> stakInt = new MyStack<int>();
             stakInt.Push(1);
@@ -19,6 +19,8 @@ namespace _11泛型
             stakInt.Pop();
             stakInt.Print();
         }
+        
+        
     }
 
     class MyStack<T>
@@ -70,4 +72,62 @@ namespace _11泛型
             }
         }
     }
+
+    class Simple
+    {
+        // 泛型方法
+        static public void ReverseAndPrint<T>(T[] arr)
+        {
+            Array.Reverse(arr);
+            foreach(T item in arr)
+            {
+                Console.Write("{0},", item.ToString());
+            }
+            Console.WriteLine("");
+        }
+    }
+
+
+    #region 扩展方法和泛型类
+    static class ExtendHoder
+    {
+        public static void Print<T>(this Holder<T> h)
+        {
+            T[] vals = h.GetValues();
+            Console.WriteLine("{0},{1},{2}", vals[0], vals[1], vals[2]);
+        }
+    }
+
+    class Holder<T>
+    {
+        T[] vals = new T[3];
+        public Holder(T v0,T v1, T v2)
+        {
+            vals[0] = v0;
+            vals[1] = v1;
+            vals[2] = v2;
+        }
+
+        public T[] GetValues()
+        {
+            return this.vals;
+        }
+    }
+
+    partial class Program
+    {
+        static void Main()
+        {
+            var intHolder = new Holder<int>(1, 3, 5);
+            intHolder.Print();
+        }
+
+    }
+    #endregion
+
+    #region 泛型结构
+
+
+    #endregion
+
 }
