92dbf5c8bb1b703b7cd936c4217b5009d07685d6
cpfyjjs <cpfyjjs@foxmail.com>
2019-02-15 17:43:26 +0800

    0215

---
+++ 11泛型/11泛型/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11泛型
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stakInt = new MyStack<int>();
+            stakInt.Push(1);
+            stakInt.Push(2);
+            stakInt.Push(4);
+            stakInt.Push(5);
+            stakInt.Print();
+            stakInt.Pop();
+            stakInt.Print();
+        }
+    }
+
+    class MyStack<T>
+    {
+        T[] StackArray;
+        int StackPoint = 0;
+
+        const int MaxStark = 10;
+
+        public MyStack()
+        {
+            StackArray = new T[MaxStark];
+        }
+
+        bool IsStackFull
+        {
+            get
+            {
+                return StackPoint >= MaxStark;
+            }
+        }
+
+        bool IsEmpty
+        {
+            get
+            {
+                return StackPoint <= 0;
+            }
+        }
+
+        public void Push(T x)
+        {
+            if (!IsStackFull)
+            {
+                StackArray[StackPoint++] = x;
+            }
+        }
+
+        public T Pop()
+        {
+            return (!IsEmpty) ? StackArray[--StackPoint] : StackArray[0];
+        }
+
+        public void Print()
+        {
+            for (int i = StackPoint - 1; i >= 0; i--)
+            {
+                Console.WriteLine(" vlalue : {0}", StackArray[i]);
+            }
+        }
+    }
+}
---
+++ 24反射和特性/24反射和特性/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace _24反射和特性
+{
+    class Program
+    {
+        public static void MyTrace(string message,
+                [CallerFilePath] string fileName="",
+                [CallerLineNumber] int lineNumber=0,
+                [CallerMemberName] string callingMember="")
+        {
+            Console.WriteLine("FileName ：{0}",fileName);
+            Console.WriteLine("LineNumber ：{0}",lineNumber);
+            Console.WriteLine("CallingMember ：{0}",callingMember);
+            Console.WriteLine("Message ：{0}",message);
+
+            }
+        static void Main(string[] args)
+        {
+            MyTrace("Simple");
+        }
+    }
+}
